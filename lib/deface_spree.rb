7abde2cc8bc345faaf2d181358c676376b997fc4
Brian Quinn <briandquinn@gmail.com>
2011-04-04 19:13:53 +0100

    Set Spree specific layout settings

--- lib/deface_spree.rb
+++ lib/deface_spree.rb
@@ -7,6 +7,8 @@ module DefaceSpree
     config.autoload_paths += %W(#{config.root}/lib)
 
     def self.activate
+      DefaceEditor::Config.editor_virtual_paths = ["shared/_head", "layouts/spree_application", "layouts/admin"]
+
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
         Rails.env.production? ? require(c) : load(c)
       end
