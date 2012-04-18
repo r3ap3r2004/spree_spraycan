c1be9a9a8c5b29d76e643b0eb138b5b3a848ff65
Brian Quinn <briandquinn@gmail.com>
2012-04-18 15:28:38 +0100

    Dropped overrides

--- app/overrides/replace_logo.rb
+++
@@ -1,7 +0,0 @@
-Deface::Override.new(:name => 'replace_logo',
-                     :virtual_path => 'spree/layouts/spree_application',
-                     :replace => 'figure#logo',
-                     :text => %q{<%= link_to root_path do %>
-                       <figure data-hook="" class="columns alpha six" id="logo"></figure>
-                    <% end %>})
-
