f25517c1fe6e0b4e50e96f79acb516939212510e
Brian D. Quinn <briandquinn@gmail.com>
2012-05-04 15:39:11 +0100

    Drop unneeded assets from installer

--- lib/generators/spree_spraycan/install/install_generator.rb
+++ lib/generators/spree_spraycan/install/install_generator.rb
@@ -2,14 +2,6 @@ module SpreeSpraycan
   module Generators
     class InstallGenerator < Rails::Generators::Base
 
-      def add_javascripts
-        append_file "app/assets/javascripts/store/all.js", "//= require spraycan/embed\n" 
-      end
-
-      def add_stylesheets
-        inject_into_file "app/assets/stylesheets/store/all.css", " *= require spraycan/embed\n", :before => /\*\//, :verbose => true
-      end
-
       def add_migrations
         run 'rake railties:install:migrations FROM=spraycan'
       end
